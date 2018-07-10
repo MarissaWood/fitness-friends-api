c6794baa4dcddcba53d02871b87b587341ee424a
Marissa Wood <marissa.espina@gmail.com>
2018-07-10 17:10:51 -0400

    index.js file

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const parser = require("body-parser");
 const app = express();
 
+const userController = require("./controllers/user.js");
+const activityController = require("./controllers/activity.js");
+const profileController = require("./controllers/profile.js");
+
 app.use(parser.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/users", userController);
+app.use("/api/activity", activityController);
+app.use("/api/profile", profileController);
+
+app.get("/", (req, res) => {
+  res.redirect("/api/activities");
+});
 
 app.set("port", process.env.PORT || 3001);
 
