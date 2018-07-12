d89845c03303e888d6ee28edf0f1923e92ce9716
Marissa Wood <marissa.espina@gmail.com>
2018-07-12 11:41:51 -0400

    removed semicolon from seed file so it runs

--- db/seedProfiles.js
+++ db/seedProfiles.js
@@ -2,12 +2,13 @@ const mongoose = require("../models/Profile.js");
 const Profile = mongoose.model("Profile");
 const profileData = require("./profile-data.json");
 
-Profile.remove({}).then(() => {
+Profile.remove({})
+  .then(() => {
     Profile.collection.insert(profileData).then(profile => {
       console.log(profile);
       process.exit();
     });
-  });
+  })
   .catch(err => {
     console.log(err);
   });
